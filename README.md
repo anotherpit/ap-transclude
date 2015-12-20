2f22013d272ce230612175580b2eea5715efd04c
anotherpit <anotherpit@gmail.com>
2015-12-21 00:25:01 +0700

    Add comments and documentation

--- README.md
+++ README.md
@@ -8,3 +8,122 @@ Why?
 ====
 
 Because [named slots for transclusion](http://blog.thoughtram.io/angular/2015/11/16/multiple-transclusion-and-named-slots.html) were introduced only in AngularJS 1.5.x, while are needed by many projects that use older versions of AngularJS. `ap-translude` does provide similar functionality for AngularJS 1.3.x.
+
+Usage
+=====
+
+Suppose, you have `myPanel` directive with regular `ngTransclude` that wraps passed markup with a `div` for styling. Declared in `myPanel.js`:
+```javascript
+myApp.directive('myPanel', function() {
+    return {
+        restrict: 'E',
+        transclude: true,
+        templateUrl: 'myPanel.html'
+    }
+});
+```
+And template in `myPanel.html`:
+```html
+<article class="my-panel" ng-transclude></article>
+```
+And used in `index.html`:
+```html
+<my-panel>
+    Hello, world!
+</my-panel>
+```
+Now, let's say you need to add some decoration: header and footer. So you rewrite your `myPanel.html`:
+```html
+<article class="my-panel">
+    <header>Header</header>
+    <ng-transclude></ng-transclude>
+    <footer>Footer</footer>
+</article>
+```
+OK? Not really. Perhaps, the user would like to be able to pass his/her own header and/or footer. That's where `apTransclude` comes for the rescue. So we rewrite `myPanel.html`:
+```html
+<article class="my-panel" ap-transclude-host>
+    <header ap-transclude-slot="header">Header</header>
+    <ng-transclude></ng-transclude>
+    <footer ap-transclude-slot="footer">Footer</footer>
+</article>
+```
+What has just happened? We declared `header` and `footer` as template slots (`apTranscludeSlot`s) so that the user can provide his/her own contents for each of them using fragments (`apTranscludeInto`s). For those slots to work we made sure that they have a common `apTranscludeHost` ancestor that is also an ancestor for `ngTransclude` node.
+Now user might use your directive in `index.html` like so:
+```html
+<my-panel>
+    Hello, world!
+    <div ap-transclude-into="header">My very special header</div>
+    <div ap-transclude-into="footer">My very special footer</div>
+</my-panel>
+```
+`apTranscludeInto`s will be removed from `ngTransclude` and placed into `apTranscludeSlot`s with the specified names, so their placement within directive doesn't matter. Thus, user might write `index.html` even so:
+```html
+<my-panel>
+    <div ap-transclude-into="footer">My very special footer</div>
+    Hello, world!
+    <div ap-transclude-into="header">My very special header</div>
+</my-panel>
+```
+If no matching slot for fragment is found, the fragment is just swallowed not affecting resulting markup. And vice versa: if there's no fragment the slot will be filled with its default markup.
+```html
+<my-panel>
+    Hello, world!
+    <div ap-transclude-into="header">My very special header</div>
+    <div ap-transclude-into="unknown">This fragment will be swallowed</div>
+</my-panel>
+```
+
+Advanced usage
+==============
+
+Now you want your header to have optional subtitle, and you decide to make it a separate directive using `apTransclude`.
+`myPanelHeader.js`
+```javascript
+myApp.directive('myPanelHeader', function() {
+    return {
+        restrict: 'E',
+        transclude: true,
+        templateUrl: 'myPanelHeader.html'
+    }
+});
+```
+`myPanelHeader.html`
+```html
+<header ap-transclude-host="header">
+    <ng-transclude></ng-transclude>
+    <div ap-transclude-slot="title">Title</div>
+    <div ap-transclude-slot="subtitle">Subtitle</div>
+</header>
+```
+Note, that we gave our host a name by passing a string value to `apTranscludeHost` attribute. Before we see it in action later, we need to rewrite `myPanel` template directive to use `myPanelHeader`.
+`myPanel.html`
+```html
+<article class="my-panel" ap-transclude-host>
+    <my-panel-header></my-panel-header>
+    <ng-transclude></ng-transclude>
+    <footer ap-transclude-slot="footer">Footer</footer>
+</article>
+```
+So we've just broken our `index.html` since there's no `header` slot available anymore. But instead we can now pass fragments inside deep nested hosts using path names like so:
+```html
+<my-panel>
+    Hello, world!
+    <div ap-transclude-into="header.title">Title</div>
+    <div ap-transclude-into="header.subtitle">Subtitle</div>
+</my-panel>
+```
+You can even change default path name separator from default `.` to whatever string you want by reassigning `nameDelimiter` property of `apTransclude.config` service:
+```index.js
+myApp.config(['myApTransclude.config', function(config) {
+    config.nameDelimiter = '$$$';
+}]);
+```
+
+Further
+=======
+
+- Each of `apTranscludeHost`, `apTranscludeInto` and `apTranscludeSlot` directives are `transluce:true` and `replace:false` so there might be some excess nodes in markup. We should investigate if we can safely use `replace:true` in conjunction with `ngRepeat`, `ngIf` and so on.
+- Looks like we can implement `apTranscludeDefault` directive to inject default slot markup inside `apTranscludeInto`.
+
+Any related contributions are welcome.
--- index.js
+++ index.js
@@ -1,65 +1,164 @@
 'use strict';
 
+/**
+ * @ngdoc module
+ * @name apTranscludes
+ */
 var module = angular.module('ap-transclude', ['ng']);
 
+/**
+ * @ngdoc service
+ * @name 'apTransclude.config'
+ *
+ * @type Object
+ * @property {string} [nameDelimiter='.']
+ */
+module.constant('apTransclude.config', {
+    nameDelimiter: '.'
+});
+
+/**
+ * @ngdoc directive
+ * @name apTranscludeHost
+ * @element ANY
+ * @restrict A
+ *
+ * @description
+ * Denotes root element in your template
+ * that contains all slots and receives all fragments:
+ * - it must be an ancestor for all `apTranscludeSlot` nodes;
+ * - it must be an ancestor for `ngTransclude` node
+ *  that is supposed to contain `apTranscludeInto` nodes when resolved;
+ *
+ * @param {string} [apTranscludeHost] Name of this host
+ */
 module.directive('apTranscludeHost', function() {
     return {
         restrict: 'A',
-        link: function($scope, $element, $attrs, controller) {
-            controller.setName($attrs.apTranscludeHost);
+        require: ['apTranscludeHost', '?^^apTranscludeHost'],
+        link: function($scope, $element, $attrs, controllers) {
+
+            var ctrl = controllers[0];
+            var parentHost = controllers[1];
+
+            if (parentHost) { ctrl.setParentHost(parentHost); }
+            $scope.$on('$destroy', function() {
+                if (parentHost) { ctrl.setParentHost(null); }
+            });
+
+            ctrl.setName($attrs.apTranscludeHost);
             $attrs.$observe('apTranscludeHost', function(name) {
-                controller.setName(name);
+                ctrl.setName(name);
             });
         },
-        controller: function() {
-
+        /**
+         * @ngdoc type
+         * @name apTranscludeHost.ApTranscludeHostController
+         */
+        controller: ['apTransclude.config', function(config) {
+            /** @type {string} */
             this._name = undefined;
-            this.setName = function(name) {
-                this._name = name;
-            };
-            this.getName = function() {
-                return this._name;
-            };
 
+            /** @type {Object.<string, apTranscludeHost.ApTranscludeHostController|null>} */
             this._parentHost = null;
-            this.setParentHost = function(host) {
-                this._parentHost = host;
-            };
-            this.getParentHost = function() {
-                return this._parentHost;
-            };
 
+            /** @type {Object.<string, apTranscludeInto.ApTranscludeIntoController|null>} */
             this._fragments = {};
+
+            /** @param {string} name */
+            this.setName = function(name) { this._name = name; };
+
+            /** @returns {string|undefined} */
+            this.getName = function() { return this._name; };
+
+            /** @param {apTranscludeHost.ApTranscludeHostController|null} host */
+            this.setParentHost = function(host) { this._parentHost = host; };
+
+            /** @returns {apTranscludeHost.ApTranscludeHostController|null} */
+            this.getParentHost = function() { return this._parentHost; };
+
+            /**
+             * @param {string} name Fragment name
+             * @param {apTranscludeInto.ApTranscludeIntoController} fragment Fragment controller
+             */
             this.setFragment = function(name, fragment) {
                 this._fragments[name] = fragment;
             };
+
+            /**
+             * Get fragment registered in this host by name
+             * @param {string} name Fragment name
+             * @returns {apTranscludeInto.ApTranscludeIntoController|null}
+             */
             this.getFragment = function(name) {
-                return this._fragments[name];
+                return this._fragments[name] || null;
             };
+
+            /**
+             * Find fragment by name starting from this host and up the hierarchy
+             *
+             * Suppose you have directive that uses transclude host named `a`,
+             * that contains another directive that uses transclude host named `b`,
+             * that contains slot named `slot`. To pass fragment for `slot`
+             * to root directive user have to name it `b.slot`. That's how
+             * the fragment will be registered in the root host.
+             *
+             * To find this template we might use any of these:
+             * - rootHostCtrl.getFragment('b.slot');
+             * - rootHostCtrl.findFragment('b.slot');
+             * - nestedHostCtrl.findFragment('slot');
+             * Though the following won't work:
+             * - nestedHostCtrl.getFragment('slot');
+             * - nestedHostCtrl.findFragment('b.slot');
+             *
+             * @param {string} name Fragment name
+             * @returns {apTranscludeInto.ApTranscludeIntoController|null}
+             */
             this.findFragment = function(name) {
                 var fragment = this.getFragment(name);
                 if (fragment) { return fragment; }
                 var parent = this.getParentHost();
                 if (!parent) { return null; }
-                return parent.findFragment(this.getName() + '.' + name);
+                return parent.findFragment([
+                    this.getName(),
+                    name
+                ].join(config.nameDelimiter));
             };
+
+            /** @param {string} name Fragment name */
             this.removeFragment = function(name) {
                 if (this._fragments[name]) {
                     delete this._fragments[name];
                 }
             };
-        }
+        }]
     }
 });
 
-
-
+/**
+ * @ngdoc directive
+ * @name apTranscludeInto
+ * @restrict A
+ * @element ANY
+ *
+ * @description
+ * Fragment to transclude into template's slot.
+ *
+ * Typically, you'll have directive that uses template
+ * that contains `apTranscludeHost` node that has several named
+ * `apTranscludeSlot`s. When you use this directive in your markup
+ * you put one or more named `apTranscludeInto's inside this directive
+ * so that they get resolved into slots.
+ *
+ * @param {string} apTranscludeInto Name of slot to transclude into
+ */
 module.directive('apTranscludeInto', function() {
     return {
         restrict: 'A',
         transclude: true,
         require: ['apTranscludeInto', '^?apTranscludeHost'],
         link: function($scope, $element, $attrs, controllers) {
+            $element.remove();
 
             var ctrl = controllers[0];
             var parentHost = controllers[1];
@@ -74,10 +173,20 @@ module.directive('apTranscludeInto', function() {
                 if (parentHost) { ctrl.setParentHost(null); }
             });
         },
-        controller: function($transclude) {
-            this.$transclude = $transclude;
+        /**
+         * @ngdoc type
+         * @name apTranscludeInto.ApTranscludeIntoController
+         */
+        controller: ['$transclude', function($transclude) {
+            this._transclude = $transclude;
 
+            /** @type {apTranscludeHost.ApTranscludeHostController|null} */
             this._parentHost = null;
+
+            /** @type {string}
+            this._name = undefined;
+
+            /** @param {apTranscludeHost.ApTranscludeHostController|null} host */
             this.setParentHost = function(host) {
                 var oldHost = this.getParentHost();
                 if (oldHost === host) { return; }
@@ -86,11 +195,11 @@ module.directive('apTranscludeInto', function() {
                 this._parentHost = host;
                 if (host) { host.setFragment(name, this); }
             };
-            this.getParentHost = function() {
-                return this._parentHost;
-            };
 
-            this._name = undefined;
+            /** @returns {apTranscludeHost.ApTranscludeHostController|null} */
+            this.getParentHost = function() { return this._parentHost; };
+
+            /** @param {string} name */
             this.setName = function(name) {
                 var oldName = this.getName();
                 if (oldName === name) { return; }
@@ -99,18 +208,31 @@ module.directive('apTranscludeInto', function() {
                 this._name = name;
                 if (parentHost) { parentHost.setFragment(name, this); }
             };
-            this.getName = function() {
-                return this._name;
-            }
-        }
+
+            /** @returns {string} */
+            this.getName = function() { return this._name; }
+        }]
     }
 });
 
+/**
+ * @ngodc directive
+ * @name apTranscludeSlot
+ * @element ANY
+ * @restrict A
+ *
+ * @description
+ * Named slot inside `apTranscludeHost` to get replaced with
+ * fragment (`apTranscludeInto` directive). If the fragment isn't provided
+ * default markup (transcluded content of `apTranscludeSlot`) is used.
+ *
+ * @param {string} apTranscludeSlot Slot name
+ */
 module.directive('apTranscludeSlot', function() {
     return {
         restrict: 'A',
         transclude: true,
-        require: ['apTranscludeSlot', '^?apTranscludeHost'],
+        require: ['apTranscludeSlot', '^^apTranscludeHost'],
         link: function($scope, $element, $attrs, controllers) {
 
             var ctrl = controllers[0];
@@ -119,16 +241,20 @@ module.directive('apTranscludeSlot', function() {
             function setName(name) {
                 if (ctrl.name === name) { return; }
                 var actualCtrl = parentHost && parentHost.findFragment(name) || ctrl;
-                actualCtrl.$transclude(function(clone) {
+                actualCtrl._transclude(function(clone) {
                     $element.empty();
                     $element.append(clone);
-                })
+                });
             }
             setName($attrs.apTranscludeSlot);
             $attrs.$observe('apTranscludeSlot', setName);
         },
-        controller: function($transclude) {
-            this.$transclude = $transclude;
-        }
+        /**
+         * @ngdoc type
+         * @name apTranscludeSlot.ApTranscludeSlotController
+         */
+        controller: ['$transclude', function($transclude) {
+            this._transclude = $transclude;
+        }]
     }
 });
